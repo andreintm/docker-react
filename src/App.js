b167a3708216231de66980e2a4a0e1ccf25f8dc9
Andrei Cociug <andrei.ntm@gmail.com>
2019-07-13 14:10:58 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!!!1
+          I was changed!!!122
         </p>
         <a
           className="App-link"
